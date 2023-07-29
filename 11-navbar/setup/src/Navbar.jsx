abb3cd8d9f732779900eaf84c60a9360dd422de8
Kenneth Delgado <kenvastolord@gmail.com>
2023-07-29 23:07:06 +0200

    toggle  menu working in navbar/setup

--- 11-navbar/setup/src/Navbar.jsx
+++ 11-navbar/setup/src/Navbar.jsx
@@ -1,29 +1,53 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
-          <img src='#' className='logo' alt='logo' />
-          <button className='nav-toggle' >
+          <img src={logo} className='logo' alt='logo' />
+          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-        <div className='links-container' ref='#'>
-          <ul className='links' ref='#'>
-            <li >
-              <a href='#'>text</a>
-            </li>
+        <div className='links-container' ref={linksContainerRef} >
+          <ul className='links' ref={linksRef} >
+            {links.map((link) => {
+              const { id, url, text } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
           </ul>
         </div>
         <ul className='social-icons'>
-          <li >
-            <a href='#'>icon</a>
-          </li>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon
+            return (
+              <li key={id}>
+                <a href={url} target='_blank' rel='noreferrer'>{icon}</a>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </nav>
