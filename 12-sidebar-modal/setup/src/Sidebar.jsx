f736473e9781e019bf24b4d59b6940b8f5def5e5
Kenneth Delgado <kenvastolord@gmail.com>
2023-07-30 10:31:56 +0200

    components working on sidebar-modal/setup

--- 12-sidebar-modal/setup/src/Sidebar.jsx
+++ 12-sidebar-modal/setup/src/Sidebar.jsx
@@ -2,28 +2,40 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
   return (
-    <aside className='sidebar show-sidebar'>
+    <aside className={`sidebar ${isSidebarOpen && 'show-sidebar'}`}>
       <div className='sidebar-header'>
-        <img src='#' className='logo' alt='coding addict' />
-        <button className='close-btn'>
+        <img src={logo} className='logo' alt='coding addict' />
+        <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
       <ul className='links'>
-        <li >
-          <a href='#'>
-            icon
-            text
-          </a>
-        </li>
+        {links.map((link) => {
+          const { id, url, text, icon } = link
+          return (
+            <li key={id} >
+              <a href={url} >
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
       </ul>
       <ul className='social-icons'>
-        <li >
-          <a href='#'>icon</a>
-        </li>
+        {social.map((socialIcon) => {
+          const { id, url, icon } = socialIcon
+          return (
+            <li key={id}>
+              <a href={url} >{icon}</a>
+            </li>
+          )
+        })}
       </ul>
     </aside>
   );
--- 12-sidebar-modal/setup/src/context.jsx
+++ 12-sidebar-modal/setup/src/context.jsx
@@ -1,1 +1,47 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, createContext } from 'react'
+
+const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        openSidebar,
+        closeSidebar,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+// Custom Hook
+
+const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider, useGlobalContext }
